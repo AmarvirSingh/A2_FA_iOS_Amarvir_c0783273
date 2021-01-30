3cf5badd7fd262bd57590baac5cf894a74e71645
Amarvir <amarvirgill06@gmail.com>
2021-01-30 23:17:14 +0530

    Initial Commit

---
+++ A2_FA_iOS_Amarvir_c0783273Tests/A2_FA_iOS_Amarvir_c0783273Tests.swift
@@ -0,0 +1,34 @@
+//
+//  A2_FA_iOS_Amarvir_c0783273Tests.swift
+//  A2_FA_iOS_Amarvir_c0783273Tests
+//
+//  Created by Amarvir Mac on 30/01/21.
+//  Copyright © 2021 Amarvir Mac. All rights reserved.
+//
+
+import XCTest
+@testable import A2_FA_iOS_Amarvir_c0783273
+
+class A2_FA_iOS_Amarvir_c0783273Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
